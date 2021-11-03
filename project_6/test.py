1f4ac8238976b3012990c3576f046b8c8e22cc32
Lyubomir Lyubenov <76185209+XNuCX@users.noreply.github.com>
2021-11-03 16:02:49 +0200

    tasks added

--- project_6/test.py
+++ project_6/test.py
@@ -1,15 +1,20 @@
 class Test:
-    def __init__(self,b, *args):
-        self.b = b
-        self.a = self.function(args)
-        for a1 in args:
-            self.a = a1
-    staticmethod
-    def function(self, args):
-        if args:
-            return args[0]
+    def __init__(self, a):
+        self._a = a
+
+    @property
+    def a(self):
+        if self._a == 0:
+            raise ValueError
         else:
-            return
-b= Test(0)
+            return self._a
+    @a.setter
+    def a(self, value):
+        self._a = value
+
+b= Test(1)
+
 print(b.a)
-print(b.b)
+
+b.a = 0
+print(b.a)
